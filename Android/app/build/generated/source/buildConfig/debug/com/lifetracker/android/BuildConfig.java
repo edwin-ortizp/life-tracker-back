7f2a67f7ef3a4ad2520fb2f876c42e094a1bae7c
edwin-ortizp <edwin.ortizp@campusucc.edu.co>
2026-01-06 18:07:05 -0500

    Android app

---
+++ Android/app/build/generated/source/buildConfig/debug/com/lifetracker/android/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lifetracker.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.lifetracker.android";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwY2p3aG5yY3Jpc2djcWpmanFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNjQwMzIsImV4cCI6MjA4Mjk0MDAzMn0.l3SKUDorBgQcIJkBdOYLtQMs0800jSLMchEILwFejJw";
+  // Field from default config.
+  public static final String SUPABASE_URL = "https://tpcjwhnrcrisgcqjfjqg.supabase.co";
+}
