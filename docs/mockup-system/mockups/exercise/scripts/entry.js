70d6f60196c85acaac35e396e6e3fa215357145b
edwin-ortizp <edwin.ortizp@campusucc.edu.co>
2026-09-13 10:12:52 -0500

    Mockup de ajustes en ejercicio + Arreglo de FAB en tareas

--- docs/mockup-system/mockups/exercise/scripts/entry.js
+++ docs/mockup-system/mockups/exercise/scripts/entry.js
@@ -1,3 +1,3 @@
 import {startMockup} from '@mockup-system/runtime/bootstrap.js';
 import {mountLifeShell} from '@project/project-assets/life-shell.js';
-startMockup({'exercise-daily': mountLifeShell, 'exercise-weekly': mountLifeShell});
+startMockup({'exercise-daily': mountLifeShell, 'exercise-weekly': mountLifeShell, 'exercise-types': mountLifeShell});
